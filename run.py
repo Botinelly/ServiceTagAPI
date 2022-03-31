d8c0d8328087f90b3bcd6334e70b77c970d300cd
Lucas Botinelly <lucas.botinelly@grupoicts.com.br>
2022-03-31 10:42:57 -0400

    updating the negocios

---
+++ run.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter, Response, FastAPI
+from random import choice
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/getServiceTagbySN")
+def getServiceTagbySN(sn: str):
+    sn = choice([sn, "8R33926O00QS", "2LCYHS2", "JQ8F2W2", "", "TR50278"])
+    media_type = choice(["text/xml", "application/json"])
+    if sn == "TR30582":
+        media_type = "text/xml"
+    if media_type == "text/xml":
+        data = '<?xml version="1.0" encoding="utf-8"?>'
+        data += f'<string xmlns="http://sfis.tpvaoc.com/COMMON">{sn}</string>'
+    else:
+        data = "ORA-12638: Credential retrieval failed"
+    return Response(content=data, media_type=media_type)
+
+
+if __name__ == "__main__":
+
+    uvicorn.run("run:app", host="0.0.0.0", port=5000, log_level="info")
